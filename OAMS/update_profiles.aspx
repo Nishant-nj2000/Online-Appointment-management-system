b6fa28e76d5ef1365c116b1905402452b1490ac8
Nishant-nj2000 <Nishant-nj2000@users.noreply.github.com>
2021-04-09 22:29:04 +0530

    update profile master page

---
+++ OAMS/update_profiles.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Dashboard.Master" CodeBehind="update_profiles.aspx.cs" Inherits="OAMS.update_profile"%>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="cpmain" runat="server">
+Helloo
+</asp:Content>
+
--- OAMS/update_profiles.aspx.cs
+++ OAMS/update_profiles.aspx.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 namespace OAMS
 {
-    public partial class Dashboard : System.Web.UI.Page
+    public partial class update_profile: System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
